78fb1de0bb8382086bd38e52b71c1bed316886b6
Easler001 <reflecter15@gmail.com>
2022-06-17 12:27:40 +0900

    追加変更1

--- env.php
+++ env.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
-define('DB_NAME', 'heroku_f4a856d89efb81f');
-define('DB_USER', 'blog_user');
-define('DB_PASS', 'abc1234');
+define('DB_HOST', "us-cdbr-east-05.cleardb.net");
+define('DB_NAME', "heroku_f4a856d89efb81f");
+define('DB_USER', 'bd34b8725da12c');
+define('DB_PASS', '59526904');
 
 ?>
