7b52ac556a8e955ccd1c521fb81bdc2e3cf78f18
Easler001 <reflecter15@gmail.com>
2022-06-17 12:23:11 +0900

    re:re

--- env.php
+++ env.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'blog_app');
+define('DB_HOST', 'us-cdbr-east-05.cleardb.net');
+define('DB_NAME', 'heroku_f4a856d89efb81f');
 define('DB_USER', 'blog_user');
 define('DB_PASS', 'abc1234');
 
